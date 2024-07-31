64086d130c008b99a7a53961fba062ac55c24140
joseantoniovegaruiz2 <79467598+joseantoniovegaruiz2@users.noreply.github.com>
2024-07-30 19:29:51 -0700

    Add files via upload

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,33 @@
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext:DbContext
+    {
+        public DataContext (DbContextOptions options): base(options){
+
+        }
+      
+     protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Addd the Postgres Extension for UUID generation
+      
+            // define configurations
+         
+        }  
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+       /// optionsBuilder.UseNpgsql("Server=localhost; Port=5432 ; User Id=admin; Password=secret");
+    //    optionsBuilder.UseNpgsql(" Server=localhost; Port=5432; User Id=admin; Password=secret; Database=auctions");
+    }
+
+
+
+        public DbSet <Product> Products {get;set;}
+        public DbSet <Seller> Sellers {get;set;}
+        public DbSet <Bid> Bids {get;set;}
+        
+        
+    }
+}
---
+++ Persistence/Migrations/20221117033057_PGInitial.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class PGInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bids",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    BidAmount = table.Column<double>(type: "double precision", nullable: false),
+                    ProductId = table.Column<string>(type: "text", nullable: false),
+                    SellerId = table.Column<string>(type: "text", nullable: false),
+                    BuyerId = table.Column<string>(type: "text", nullable: true),
+                    category = table.Column<string>(type: "text", nullable: true),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bids", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ProductName = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: false),
+                    ShortDescription = table.Column<string>(type: "text", nullable: false),
+                    DetailedDescription = table.Column<string>(type: "text", nullable: false),
+                    Category = table.Column<string>(type: "text", nullable: false),
+                    StartingPrice = table.Column<string>(type: "text", nullable: false),
+                    BidEnDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sellers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FirstName = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: false),
+                    LastName = table.Column<string>(type: "character varying(25)", maxLength: 25, nullable: false),
+                    Address = table.Column<string>(type: "text", nullable: false),
+                    City = table.Column<string>(type: "text", nullable: false),
+                    State = table.Column<string>(type: "text", nullable: false),
+                    Pin = table.Column<string>(type: "text", nullable: false),
+                    Phone = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sellers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bids");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Sellers");
+        }
+    }
+}
